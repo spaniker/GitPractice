ca14e8457eb3ac830bbf38ed5fc5bf64ecff8f28
spaniker <84048769+spaniker@users.noreply.github.com>
2025-07-23 11:03:25 -0700

    Initial Commit

---
+++ GitPractice/GitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitPracticeApp.swift
+//  GitPractice
+//
+//  Created by Syona Paniker on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

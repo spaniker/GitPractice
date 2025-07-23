23c006a340ef8468b2088f9c4840b11e5acf6da1
spaniker <84048769+spaniker@users.noreply.github.com>
2025-07-23 11:25:02 -0700

    more text

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("I love purple")
+            Text("HI")
         }
         .padding()
     }
